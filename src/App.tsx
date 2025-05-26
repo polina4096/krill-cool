5a64e5089910e81269adb53fcd14be95b94946dc
polina4096 <polina4096@yandex.ru>
2025-05-27 02:38:06 +0400

    Design improvements, responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
+import krill from "./assets/krill.jpg";
 import logo from "./assets/logo.gif";
 
 export function App() {
   return (
     <div class="h-screen flex items-center justify-center">
-      <img src={logo} class="w-[75%] max-w-screen-lg" />
+      <div class="w-[80%]">
+        <img src={krill} class="max-w-[320px] w-full pr-4" />
+        <img src={logo} class="max-w-[600px] w-full" />
+      </div>
     </div>
   );
 }
